e9c42a4c2806c58b0beb2c08fb4e284f3a0adc30
margaridafelgueirasgoncalves <margaridafelgueirasgoncalves@gmail.com>
2021-04-08 19:39:36 +0100

    changed configs

--- ProjectByeBye/src/main/java/org/academiadecodigo/asycntomatics/byebye/dao/userDaoImp.java
+++ ProjectByeBye/src/main/java/org/academiadecodigo/asycntomatics/byebye/dao/userDaoImp.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.asycntomatics.byebye.dao;
 
 import org.academiadecodigo.asycntomatics.byebye.model.User;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,6 +9,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
+@Repository
 public class userDaoImp extends GenericDao<User> implements UserDao {
 
 
